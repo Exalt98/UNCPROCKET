209abcc190f6f298dde08807ca47a297f10a3ba0
Ben <brs012@bravemail.uncp.edu>
2022-03-19 14:29:05 -0400

    Move output file to /boot/

--- apps/BaremetricP/run.sh
+++ apps/BaremetricP/run.sh
@@ -1,2 +1,3 @@
-python /home/pi/bmp388/UNCPROCKET.git/apps/BaremetricP/bmp388.py > $(/home/pi/bmp388/UNCPROCKET.git/apps/scripts/timestamp)_barimetric.txt
+set -x
+nohup python /home/pi/bmp388/UNCPROCKET.git/apps/BaremetricP/bmp388.py > /boot/$(/home/pi/bmp388/UNCPROCKET.git/apps/scripts/timestamp)_barimetric.txt
 

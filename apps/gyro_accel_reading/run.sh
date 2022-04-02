b70d691ef624fe47d1418f37af9d83351f312342
Ben <brs012@bravemail.uncp.edu>
2022-04-02 14:53:26 -0400

    Add gyro_accel_reading/run.sh

---
+++ apps/gyro_accel_reading/run.sh
@@ -0,0 +1,5 @@
+set -x
+
+nohup $(/home/pi/bmp388/BerryIMU/gyro_accelerometer_tutorial01_angles/gyro_accelerometer_tutorial01 > /boot/$(/home/pi/bmp388/UNCPROCKET.git/apps/scripts/timestamp)_gyro_accel.txt) > /dev/null
+
+exit 0

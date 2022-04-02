f9862525998b96b06cb179c151666900131c7f29
Ben <brs012@bravemail.uncp.edu>
2022-04-02 18:36:05 -0400

    Fix bug where barimetric log contains 0 bytes

--- apps/BaremetricP/run.sh
+++ apps/BaremetricP/run.sh
@@ -1,3 +1,5 @@
 set -x
-nohup python /home/pi/bmp388/UNCPROCKET.git/apps/BaremetricP/bmp388.py > /boot/$(/home/pi/bmp388/UNCPROCKET.git/apps/scripts/timestamp)_barimetric.txt
 
+nohup $(python /home/pi/bmp388/UNCPROCKET.git/apps/BaremetricP/bmp388.py > /boot/$(/home/pi/bmp388/UNCPROCKET.git/apps/scripts/timestamp)_barimetric.txt) > /dev/null
+
+exit 0
